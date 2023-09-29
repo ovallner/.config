1dda19df1cb3a08281f51e283446f5113dd8575d
Oscar.Vallner <oscar.vallner@credera.com>
2023-09-29 15:14:48 -0600

    setting up

---
+++ zsh/aliases.zsh
@@ -0,0 +1,17 @@
+# git aliases
+alias gc="git commit -m "
+alias ga="git add ."
+alias gp="git push origin HEAD"
+alias gs="git status"
+
+# project aliases
+alias goarmy="cd ~/Development/army/repos"
+alias svor="cd ~/Development/army/repos/army-tddb-ap-pyt-svor"
+alias ap-retry="cd ~/Development/army/repos/army-tddb-ap-pyt-retry-handler"
+alias lp-retry="cd ~/Development/army/repos/army-tddb-lp-pyt-retry-handler"
+alias manifest="cd ~/Development/army/repos/army-tddb-cf-manifest"
+
+alias vimconf="cd ~/.config/nvim"
+
+# nvim for vim
+alias vim="nvim"

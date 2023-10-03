43daf5b01f25554fa65bc60edd7113ce4540a3d0
Oscar.Vallner <oscar.vallner@credera.com>
2023-10-03 15:45:55 -0600

    changing telescope grep and setting keybinds for split windows

--- nvim/lua/oscar/remap.lua
+++ nvim/lua/oscar/remap.lua
@@ -4,3 +4,6 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set("n", "<C-;>", "<C-w><C-s>")
+vim.keymap.set("n", "<C-'>", "<C-w><C-v>")

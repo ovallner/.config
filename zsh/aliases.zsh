cb1affe9a359954eefa49a4eb148c017d99c288d
Oscar.Vallner <oscar.vallner@credera.com>
2025-01-07 15:29:32 -0700

    adding aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,8 +3,10 @@ alias gc="git commit -m "
 alias ga="git add ."
 alias gp="git push origin HEAD"
 alias gs="git status"
+alias gac="git commit -am "
 
 alias vimconf="cd ~/.config/nvim"
 
 # nvim for vim
 alias vim="nvim"
+alias python=python3
